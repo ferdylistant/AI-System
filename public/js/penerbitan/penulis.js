f97d1b4f68d09b692c5e348c4714aa7664447f1f
ferdylistant <ferdylucker@.com>
2023-09-06 15:45:51 +0700

    Update Penulis

--- public/js/penerbitan/penulis.js
+++ public/js/penerbitan/penulis.js
@@ -16,21 +16,21 @@ $(function () {
                         extend: 'print',
                         text: '<i class="text-secondary fa fa-print"></i> Print',
                         exportOptions: {
-                            columns: [0,1,2,4,5,6,7,8,9]
+                            columns: [0,1,2,3]
                         }
                     },
                     {
                         extend: 'pdf',
                         text: '<i class="text-danger fa fa-file-pdf"></i> PDF',
                         exportOptions: {
-                            columns: [0,1,2,4,5,6,7,8,9]
+                            columns: [0,1,2,3]
                         }
                     },
                     {
                         extend: 'excel',
                         text: '<i class="text-success fa fa-file-excel"></i> Excel',
                         exportOptions: {
-                            columns: [0,1,2,4,5,6,7,8,9]
+                            columns: [0,1,2,3]
                         }
                     },
                 ]
