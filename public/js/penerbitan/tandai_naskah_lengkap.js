0052b734f33753687db0ff967b47d1a4000edc6e
ferdylistant <ferdylucker@gmail.com>
2024-01-19 10:51:53 +0700

    Filter revisi

--- public/js/penerbitan/tandai_naskah_lengkap.js
+++ public/js/penerbitan/tandai_naskah_lengkap.js
@@ -155,91 +155,7 @@ $(document).ready(function () {
     //     var info = tableNaskah.page.info();
     //     console.log(info.pages);
     // });
-    $('[name="status_filter_jb"]').on('change', function () {
-        var val = $.fn.dataTable.util.escapeRegex($(this).val());
-        tableNaskah.column($(this).data('column'))
-            .search(val ? val : '', true, false)
-            .draw();
-        cardWrap = $(this).closest(".card");
-        if (val == "Reguler") {
-            $.ajax({
-                url: baseUrl + "?request_=getCountNaskahReguler",
-                type: "GET",
-                dataType: "JSON",
-                beforeSend: function () {
-                    cardWrap.addClass("card-progress");
-                },
-                success: function (data) {
-                    // console.log(data);
-                    $("#countPenilaian").attr("hidden", false);
-                    $("#countPenilaian").html(data.html);
-                    $("#naskahBelumDinilai").prop('Counter', 0).animate({
-                        Counter: data.belumDinilai
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now) + ' Naskah');
-                        }
-                    });
-                    $("#animate-count-prodev").prop('Counter', 0).animate({
-                        Counter: data.countProdev
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                    $("#animate-count-mpenerbitan").prop('Counter', 0).animate({
-                        Counter: data.countMPenerbitan
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                    $("#animate-count-mpemasaran").prop('Counter', 0).animate({
-                        Counter: data.countMPemasaran
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                    $("#animate-count-dpemasaran").prop('Counter', 0).animate({
-                        Counter: data.countDPemasaran
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                    $("#animate-count-direksi").prop('Counter', 0).animate({
-                        Counter: data.countDireksi
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                },
-                complete: function () {
-                    cardWrap.removeClass("card-progress");
-                }
-            });
-        } else {
-            cardWrap.addClass("card-progress");
-            setTimeout(function () {
-                cardWrap.removeClass("card-progress");
-            }, 1000);
-            $("#countPenilaian").attr("hidden", true);
-        }
-    });
+
     var checkBoxes = $('input:checkbox');
     checkBoxes.change(function () {
         $('button[type="submit"]').prop('disabled', checkBoxes.filter(':checked').length < 1);
@@ -280,7 +196,7 @@ $(document).ready(function () {
                 if ($.fn.DataTable.isDataTable('#tb_Naskah')) {
                     $('#tb_Naskah').DataTable().destroy();
                 }
-                $('#tb_Naskah').DataTable({
+                let tableNaskah = $('#tb_Naskah').DataTable({
                     // "bSort": false,
                     "responsive": true,
                     "autoWidth": false,
@@ -518,6 +434,91 @@ $(document).ready(function () {
                 'print', 'excel', 'pdf'
             ]
         });
+    });
+    $('[name="status_filter_jb"]').on('change', function () {
+        var val = $.fn.dataTable.util.escapeRegex($(this).val());
+        tableNaskah.column($(this).data('column'))
+            .search(val ? val : '', true, false)
+            .draw();
+        cardWrap = $(this).closest(".card");
+        if (val == "Reguler") {
+            $.ajax({
+                url: baseUrl + "?request_=getCountNaskahReguler",
+                type: "GET",
+                dataType: "JSON",
+                beforeSend: function () {
+                    cardWrap.addClass("card-progress");
+                },
+                success: function (data) {
+                    // console.log(data);
+                    $("#countPenilaian").attr("hidden", false);
+                    $("#countPenilaian").html(data.html);
+                    $("#naskahBelumDinilai").prop('Counter', 0).animate({
+                        Counter: data.belumDinilai
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now) + ' Naskah');
+                        }
+                    });
+                    $("#animate-count-prodev").prop('Counter', 0).animate({
+                        Counter: data.countProdev
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                    $("#animate-count-mpenerbitan").prop('Counter', 0).animate({
+                        Counter: data.countMPenerbitan
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                    $("#animate-count-mpemasaran").prop('Counter', 0).animate({
+                        Counter: data.countMPemasaran
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                    $("#animate-count-dpemasaran").prop('Counter', 0).animate({
+                        Counter: data.countDPemasaran
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                    $("#animate-count-direksi").prop('Counter', 0).animate({
+                        Counter: data.countDireksi
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                },
+                complete: function () {
+                    cardWrap.removeClass("card-progress");
+                }
+            });
+        } else {
+            cardWrap.addClass("card-progress");
+            setTimeout(function () {
+                cardWrap.removeClass("card-progress");
+            }, 1000);
+            $("#countPenilaian").attr("hidden", true);
+        }
     });
     $('[name="status_filter"]').on('change', function () {
         var val = $.fn.dataTable.util.escapeRegex($(this).val());
