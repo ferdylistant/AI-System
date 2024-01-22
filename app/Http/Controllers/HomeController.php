ea50a2986ffba2b4710860982881280d1f50c4be
ferdylistant <ferdylucker@.com>
2024-01-22 08:25:27 +0700

    Recent Login Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -247,9 +247,9 @@ class HomeController extends Controller
                 ->join('jabatan as j', 'j.id', '=', 'u.jabatan_id')
                 ->join('divisi as d', 'd.id', '=', 'u.divisi_id')
                 ->whereDate('ul.last_login', Carbon::today())
-                ->select('ul.*', 'u.nama', 'u.email', 'u.avatar', 'j.nama as jabatan', 'd.nama as divisi')
-                ->orderBy('ul.last_login', 'desc')
                 ->groupBy('u.id')
+                ->orderBy('ul.last_login', 'desc')
+                ->select('ul.*', 'u.nama', 'u.email', 'u.avatar', 'j.nama as jabatan', 'd.nama as divisi')
                 ->paginate(4);
             foreach ($data as $key => $d) {
                 if (auth()->id() == $d->users_id) {
