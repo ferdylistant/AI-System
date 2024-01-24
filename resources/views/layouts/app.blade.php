db3cc84828fc4ee3cb10df186e51bf6cb84eb8b9
ferdylistant <ferdylucker@.com>
2024-01-24 15:06:15 +0700

    Update User Detail Tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ $title }} &mdash; Andi Intelligent System</title>
     <!-- Google Tag Manager -->
-    <script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NRG27JKQ');</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J3486GFGFF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J3486GFGFF');
+</script>
+
     <!-- End Google Tag Manager -->
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ url('vendors/bootstrap/dist/css/bootstrap.min.css') }}">
@@ -178,9 +183,9 @@
 </head>
 
 <body>
-    <!-- Google Tag Manager (noscript) -->
+    {{-- <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRG27JKQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> --}}
     <!-- End Google Tag Manager (noscript) -->
     <header class="header">
         <div id="autocomplete"></div>
