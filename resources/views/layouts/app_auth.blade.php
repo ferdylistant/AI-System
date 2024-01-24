aeea7f21bd5de7fcfe5a55a59db5ff053b2f751e
ferdylistant <ferdylucker@.com>
2024-01-24 13:12:18 +0700

    Update User Detail Tab

--- resources/views/layouts/app_auth.blade.php
+++ resources/views/layouts/app_auth.blade.php
@@ -8,7 +8,13 @@
     <meta name="description" content="AI System (Andi Intelligent System)">
     <meta name="author" content="Andi Global Soft">
     <title>{{$title}} &mdash; AI System</title>
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NRG27JKQ');</script>
+    <!-- End Google Tag Manager -->
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ url('vendors/bootstrap/dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('vendors/font-awesome/css/all.css') }}">
@@ -24,6 +30,10 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRG27JKQ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @yield('content')
     <!-- General JS Scripts -->
     <script type="text/javascript" src="{{ url('vendors/jquery/dist/jquery.js') }}"></script>
