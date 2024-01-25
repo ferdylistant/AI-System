ba9e915b53a730635de3e49e7a86e2dd4d78b974
ferdylistant <ferdylucker@.com>
2024-01-25 14:54:25 +0700

    Header Security

--- app/Http/Middleware/Security.php
+++ app/Http/Middleware/Security.php
@@ -27,6 +27,12 @@ class Security
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubdomains',true);
             $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
             $response->headers->set('X-XSS-Protection', '1; mode=block');
+            $response->headers->set('X-Frame-Options', 'SAMEORIGIN', false);
+            $response->headers->set('Expect-CT', 'enforce, max-age=30');
+            $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token');
             // $response->headers->set('Content-Security-Policy', "
             // default-src *;
             // script-src 'self' platform.twitter.com plausible.io utteranc.es unpkg.com *.cloudflare.com 'unsafe-inline' 'unsafe-eval' plausible.io/js/plausible.js utteranc.es/client.js;
@@ -37,11 +43,6 @@ class Security
             // frame-src 'self' platform.twitter.com plausible.io utteranc.es github.com *.youtube.com *.vimeo.com;
             // object-src 'none'; base-uri 'self';");
             // $response->headers->set('Content-Security-Policy', "default-src *; style-src * 'unsafe-inline'; script-src 'self' platform.twitter.com plausible.io utteranc.es *.cloudflare.com 'unsafe-inline' 'unsafe-eval' plausible.io/js/plausible.js utteranc.es/client.js; style-src 'self' *.cloudflare.com 'unsafe-inline'; img-src 'self' * data:; font-src 'self' data: ; connect-src 'self' plausible.io/api/event; media-src 'self'; frame-src 'self' platform.twitter.com plausible.io utteranc.es github.com *.youtube.com *.vimeo.com; object-src 'none'; base-uri 'self';");
-            $response->headers->set('Expect-CT', 'enforce, max-age=30');
-            $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token');
 
             $this->removeUnwantedHeaders($this->unwantedHeaders);
         }
