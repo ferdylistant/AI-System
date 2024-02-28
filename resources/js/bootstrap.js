8466a38b8ff893459b3d9846de78f979ebbde35e
ferdylistant <ferdylucker@.com>
2024-02-28 15:28:25 +0700

    Websocket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -27,7 +27,7 @@ window.Echo = new Echo({
     encrypted: false,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
+    wssPort: 6000,
     disableStats: true,
     forceTLS: true,
     enabledTransports: ['ws', 'wss'],
