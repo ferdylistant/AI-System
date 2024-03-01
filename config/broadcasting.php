bd29cff26673f9eddead2754c3c33d5aee1f47eb
ferdylistant <ferdylucker@gmail.com>
2024-03-01 23:17:49 +0700

    Broadcasting config changed

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'encrypted' => true,
+                'encrypted' => true,
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_PORT', 6001),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
