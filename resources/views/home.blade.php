2a0d1b086ff243be352707947d3ff669f546e987
ferdylistant <ferdylucker@gmail.com>
2024-03-02 11:55:26 +0700

    Broadcasting config changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -555,17 +555,17 @@
         document.addEventListener("DOMContentLoaded", function(event) {
             Pusher.logToConsole = true;
 
-            var pusher = new Pusher('{{env('PUSHER_APP_KEY')}}', {
-            cluster: '{{env('PUSHER_APP_CLUSTER')}}',
-            encrypted: true
-            });
-            var channel = pusher.subscribe('hello-channel');
-            channel.bind('.test.notification', function(data) {
-            alert(JSON.stringify(data));
-            });
-            // Echo.channel(`hello-channel`).listen('.test.notification', (data) => {
-            //     alert(JSON.stringify(data));
+            // var pusher = new Pusher('{{env('PUSHER_APP_KEY')}}', {
+            // cluster: '{{env('PUSHER_APP_CLUSTER')}}',
+            // encrypted: true
             // });
+            // var channel = pusher.subscribe('hello-channel');
+            // channel.bind('.test.notification', function(data) {
+            // alert(JSON.stringify(data));
+            // });
+            Echo.channel(`hello-channel`).listen('.test.notification', (data) => {
+                alert(JSON.stringify(data));
+            });
         });
     </script>
     {{-- <script>
