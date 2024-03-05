64a8bff97dca0674dbef3d70698a2353c4b8f2df
ferdylistant <ferdylucker@.com>
2024-03-05 15:27:26 +0700

    Update Filter Daterange

--- app/Http/Controllers/WebSocketController.php
+++ app/Http/Controllers/WebSocketController.php
@@ -9,8 +9,8 @@ class WebSocketController extends Controller
 {
     public function index() {
         $data = 'Data coba-coba';
-        header("HTTP/1.1 200 OK");
         broadcast(new TesWebsocketEvent($data));
+        return;
 
     }
 }
