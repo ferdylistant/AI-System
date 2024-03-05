f12725e1c12a8a39f700efd25fff985b8fd9e92b
ferdylistant <ferdylucker@.com>
2024-03-05 15:23:45 +0700

    Update Filter Daterange

--- app/Events/TesWebsocketEvent.php
+++ app/Events/TesWebsocketEvent.php
@@ -9,9 +9,9 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class TesWebsocketEvent implements ShouldBroadcast
+class TesWebsocketEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
